47806d676a25f78aefabf366628864ba10cbf39e
JosafatGambino <kevin.hernandezg17@tectijuana.edu.mx>
2020-11-23 13:38:11 -0800

    Program correction

--- examples/GradientBoostingClassificationExample.scala
+++ examples/GradientBoostingClassificationExample.scala
@@ -1,71 +1,43 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.{GBTClassificationModel, GBTClassifier}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
 
-// scalastyle:off println
-package org.apache.spark.examples.mllib
+// Load and parse the data file, converting it to a DataFrame.
+val data = spark.read.format("libsvm").load("sample_libsvm_data.txt")
 
-import org.apache.spark.{SparkConf, SparkContext}
-// $example on$
-import org.apache.spark.mllib.tree.GradientBoostedTrees
-import org.apache.spark.mllib.tree.configuration.BoostingStrategy
-import org.apache.spark.mllib.tree.model.GradientBoostedTreesModel
-import org.apache.spark.mllib.util.MLUtils
-// $example off$
+// Index labels, adding metadata to the label column.
+// Fit on whole dataset to include all labels in index.
+val labelIndexer = new StringIndexer().setInputCol("label").setOutputCol("indexedLabel").fit(data)
+// Automatically identify categorical features, and index them.
+// Set maxCategories so features with > 4 distinct values are treated as continuous.
+val featureIndexer = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(4).fit(data)
 
-object GradientBoostingClassificationExample {
-  def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("GradientBoostedTreesClassificationExample")
-    val sc = new SparkContext(conf)
-    // $example on$
-    // Load and parse the data file.
-    val data = MLUtils.loadLibSVMFile(sc, "data/mllib/sample_libsvm_data.txt")
-    // Split the data into training and test sets (30% held out for testing)
-    val splits = data.randomSplit(Array(0.7, 0.3))
-    val (trainingData, testData) = (splits(0), splits(1))
+// Split the data into training and test sets (30% held out for testing).
+val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
 
-    // Train a GradientBoostedTrees model.
-    // The defaultParams for Classification use LogLoss by default.
-    val boostingStrategy = BoostingStrategy.defaultParams("Classification")
-    boostingStrategy.numIterations = 3 // Note: Use more iterations in practice.
-    boostingStrategy.treeStrategy.numClasses = 2
-    boostingStrategy.treeStrategy.maxDepth = 5
-    // Empty categoricalFeaturesInfo indicates all features are continuous.
-    boostingStrategy.treeStrategy.categoricalFeaturesInfo = Map[Int, Int]()
+// Train a GBT model.
+val gbt = new GBTClassifier().setLabelCol("indexedLabel").setFeaturesCol("indexedFeatures").setMaxIter(10).setFeatureSubsetStrategy("auto")
 
-    val model = GradientBoostedTrees.train(trainingData, boostingStrategy)
+// Convert indexed labels back to original labels.
+val labelConverter = new IndexToString().setInputCol("prediction").setOutputCol("predictedLabel").setLabels(labelIndexer.labels)
 
-    // Evaluate model on test instances and compute test error
-    val labelAndPreds = testData.map { point =>
-      val prediction = model.predict(point.features)
-      (point.label, prediction)
-    }
-    val testErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / testData.count()
-    println(s"Test Error = $testErr")
-    println(s"Learned classification GBT model:\n ${model.toDebugString}")
+// Chain indexers and GBT in a Pipeline.
+val pipeline = new Pipeline().setStages(Array(labelIndexer, featureIndexer, gbt, labelConverter))
 
-    // Save and load model
-    model.save(sc, "target/tmp/myGradientBoostingClassificationModel")
-    val sameModel = GradientBoostedTreesModel.load(sc,
-      "target/tmp/myGradientBoostingClassificationModel")
-    // $example off$
+// Train model. This also runs the indexers.
+val model = pipeline.fit(trainingData)
 
-    sc.stop()
-  }
-}
-// scalastyle:on println
+// Make predictions.
+val predictions = model.transform(testData)
 
+// Select example rows to display.
+predictions.select("predictedLabel", "label", "features").show(20)
 
+// Select (prediction, true label) and compute test error.
+val evaluator = new MulticlassClassificationEvaluator().setLabelCol("indexedLabel").setPredictionCol("prediction").setMetricName("accuracy")
+val accuracy = evaluator.evaluate(predictions)
+println(s"Test Error = ${1.0 - accuracy}")
+
+val gbtModel = model.stages(2).asInstanceOf[GBTClassificationModel]
+println(s"Learned classification GBT model:\n ${gbtModel.toDebugString}")
